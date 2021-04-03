75629fc3cd662d73fdceec3a144525466b78c895
Gabriel Costa <gabrielestevan14@gmail.com>
2021-04-03 16:59:03 -0300

    Listing of users

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -6,10 +6,23 @@ interface IRequest {
 }
 
 class ListAllUsersUseCase {
-  constructor(private usersRepository: IUsersRepository) {}
+  // eslint-disable-next-line prettier/prettier
+  constructor(private usersRepository: IUsersRepository) { }
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const userExists = this.usersRepository.findById(user_id);
+
+    if (!userExists) {
+      throw new Error("User does not exists!");
+    }
+
+    if (!userExists.admin) {
+      throw new Error("User not admin!");
+    }
+
+    const user = this.usersRepository.list();
+
+    return user;
   }
 }
 
